5ef1c6b7f6ba880f4d4b6721d3664dd567e33be9
yurim <kyl6682@gmail.com>
2025-03-19 14:42:49 +0900

    🔒️ AccessToken으로 유저 정보 관리

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -42,7 +42,9 @@ app.post("/", (req, res) => {
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
     // 이곳에 코드를 작성하세요.
+     const accessToken = jwt.sign({userId : userInfo.user_id}, secretKey)
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
+    res.send(accessToken)
     // 이곳에 코드를 작성하세요.
   }
 });
@@ -51,8 +53,12 @@ app.post("/", (req, res) => {
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
   // 이곳에 코드를 작성하세요.
+  const accessToken = req.headers.authorization.split(' ')[1]
+  const payload = jwt.verify(accessToken, secretKey)
+  const userInfo = users.find(el => el.user_id === payload.userId)
   // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
   // 이곳에 코드를 작성하세요.
+  return res.json(userInfo)
 });
 
 app.listen(3000, () => console.log("서버 실행!"));
